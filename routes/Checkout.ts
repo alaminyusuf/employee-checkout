918adfe6e65786c72b9b3429f4fb0f069692c408
alaminyusuf <alaminyusuf131@gmail.com>
2024-12-12 18:55:37 +0100

    Added route for handling authentication and it's UI

--- routes/Checkout.ts
+++ routes/Checkout.ts
@@ -4,8 +4,14 @@ import CheckoutController from "../controller/Checkout";
 const router = Router();
 const controller = new CheckoutController();
 
-router.post("/checkout", (req: Request, res: Response) => {
-	return controller.check(req, res);
+router.post("/", async (req: Request, res: Response) => {
+	return await controller.authenticate(req, res);
+});
+
+router.get("/", (_req: Request, res: Response) => {
+	res.render("auth", {
+		title: "Employee Checkout",
+	});
 });
 
 export default router;
